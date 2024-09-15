4fbdc161ddcd7b7148415c0a76f744f76f636593
wycky1990 <wyclifffeomondi@gmail.com>
2024-09-15 05:59:12 -0700

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,11 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <Python.h>
+
 /**
- * print_python_list_info -  function that prints some basic
- *							info about Python lists
+ * print_python_list_info -function that prints some basic
+ * info about Python lists
  * @p: python list
  */
+
 void print_python_list_info(PyObject *p)
 {
 	int elem;
